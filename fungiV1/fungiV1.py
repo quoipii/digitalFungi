59829eaeb27e0a66fb0bac91852609188fc4ef8d
quoipi <45893478+quoipii@users.noreply.github.com>
2022-07-24 00:21:11 +1000

    Update fungiV1.py
    Changed the branch rate to be more effecient. Program now works much more efficiently with higher generation and initHyphae values.

--- fungiV1/fungiV1.py
+++ fungiV1/fungiV1.py
@@ -3,15 +3,21 @@ import random
 import math
 from pathlib import Path
 
-generations = 150
+generations = 130
 initHyphae = 3
 lineLen = 5
 imgDims = (1000,1000)
+endPointsNum = 580
 
+allLines = []
 openEnds = []
 
 #Gets the y-value of angle2 on a bell curve that peaks at angle1 (used for moveList weights)
 bellCurve = lambda angle1, angle2: 200*math.e**((-(angle2-angle1)**2)/1000)
+
+#Gets the rate of branching for given initHyphae and generations values. Based entirely around how many endPoints you think there should be in the final state.
+#Trust me on this. Spent hours calculating how to find an equation for the rate that would make it always the optimal amount.
+branchRate = round(1/(((endPointsNum/initHyphae)**(1/(generations-1)))-1))
 
 #Makes lines go from thick to thin as they grow further
 hyphaeThickness = lambda forLoopMax, forLoopPos: round(((forLoopMax-forLoopPos)+1)/(0.1*forLoopMax))
@@ -41,7 +47,7 @@ def getCoordsBranch(prevAngle, coords):
     coordRight = random.choices(moveList, weights=weightListRight)[0]
     return [coordLeft, coordRight], [angleList[moveList.index(coordLeft)], angleList[moveList.index(coordRight)]]
 
-background = Image.new(mode="RGB", size=imgDims, color="white")
+background = Image.new(mode="RGBA", size=imgDims, color="black")
 frames = []
 print(f"Mycelium will branch approximately {((initHyphae-1)*10)*generations} times")
 
@@ -50,7 +56,7 @@ for i in range(0, initHyphae):
     #Get end coords then draw the hyphae
     startCoords = (round(imgDims[0]/2), round(imgDims[0]/2))
     endCoords, angle = getCoordsSingle(0, startCoords, True)
-    ImageDraw.Draw(background).line([startCoords, endCoords], fill=(0,0,0,255), width=hyphaeThickness(initHyphae, i))
+    ImageDraw.Draw(background).line([startCoords, endCoords], fill=(255,255,round(255-(i/generations)*255),255), width=hyphaeThickness(initHyphae, i))
     openEnds.append([(endCoords), angle])
 
     frames.append(background.copy())
@@ -61,16 +67,19 @@ for i in range(0, generations):
     for startCoords in openEndsDup:
         openEnds.remove(startCoords)
 
-        if random.choice(range(0,round((initHyphae-1)*10))) == 2:
+        if random.choice(range(0,branchRate)) == 2:
             LRcoords, LRangles = getCoordsBranch(startCoords[1], startCoords[0])
-            ImageDraw.Draw(background).line([startCoords[0], LRcoords[0]], fill=(0,0,0,255), width=hyphaeThickness(generations, i))
-            ImageDraw.Draw(background).line([startCoords[0], LRcoords[1]], fill=(0,0,0,255), width=hyphaeThickness(generations, i))
-            openEnds.append([LRcoords[0], LRangles[0]])
-            openEnds.append([LRcoords[1], LRangles[1]])
-
+            
+            #Draw and record both the left and right lines
+            for j in range(0, 2):
+                ImageDraw.Draw(background).line([startCoords[0], LRcoords[j]], fill=(255,255,round(255-(i/generations)*255),255), width=hyphaeThickness(generations, i))
+                allLines.append([startCoords[0], LRcoords[j]])
+                openEnds.append([LRcoords[j], LRangles[j]])
+                
         else:
             endCoords, angle = getCoordsSingle(startCoords[1], startCoords[0], False)
-            ImageDraw.Draw(background).line([startCoords[0], endCoords], fill=(0,0,0,255), width=hyphaeThickness(generations, i))
+            ImageDraw.Draw(background).line([startCoords[0], endCoords], fill=(255,255,round(255-(i/generations)*255),255), width=hyphaeThickness(generations, i))
+            allLines.append([startCoords[0], endCoords])
             openEnds.append([endCoords, angle])
 
     if (i/generations)*100 % 2 == 0:
@@ -81,5 +90,4 @@ for i in range(0, generations):
 durationList = [100 for i in range(0,len(frames)-1)]
 durationList.append(2000)
 
-frames[0].save(f'{Path.cwd()}\\mycGif.gif', save_all=True, append_images=frames[1:], optimize=False, duration=durationList, loop=0)
-background.show()
+frames[0].save(f'{Path.cwd()}\\mushGif.gif', save_all=True, append_images=frames[1:], optimize=False, duration=durationList, loop=0)
